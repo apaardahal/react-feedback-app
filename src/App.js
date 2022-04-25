e0cdb6962fc11fd394b345b758c16c12e4103f32
apaardahal <52095639+apaardahal@users.noreply.github.com>
2022-04-25 21:53:20 +0545

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
     return ( 
         <>
             <Header/>
+            <FeedbackItem/>
         </> 
         )
     }
